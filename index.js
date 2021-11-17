dc851c1c525510ccf862ff57e0e23eed59739ee0
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-11-17 18:18:44 +0100

    final commit for database

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+var corsOptions = {
+    origin: "https://localhost:4000" || "",
+    optionsSuccessStatus: 204
+};
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serveur à l'écoute sur le port ${port} !`);
+});
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("Le serveur est à l'écoute.");
+})
+
+require('./routes')(app);
