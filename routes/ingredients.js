47b4051a57e5af78ebb48e9f58159b29ad9a24e3
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-11-21 17:38:46 +0100

    Untested put for ingredients and untested put for adding stocks.

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -23,4 +23,30 @@ router.post('/post', function (req, res){
     res.status(200).send('Ingredient has been added successfully.');
 });
 
+// requires input names code, libelle, unit, unitprice, stocks, stockvalue and allergene - make sure stocks are up to date (ie. current + new)
+router.put('/put/:ingredientId', function (req, res){
+    let ingredient = req.params.ingredientId;
+    let update = req.body;
+    database.query(`UPDATE ingredients SET libelle="${update.libelle}"unit=,"${update.unit}",unitprice=${update.unitprice},stocks=${update.stocks},stockvalue=${update.stockvalue},allergene=${update.allergene} WHERE code=${ingredient};`, function(err, result){
+        if(err) throw err;
+        console.log("Update complete.");
+    });
+    res.status(200).send('Ingredient has been updated successfully.');
+});
+
+// requires input names code, libelle, unit, unitprice, stocks, stockvalue and allergene - make sure stocks are up to date (ie. current + new)
+router.put('/addstock/:ingredientId', function (req, res){
+    let ingredient = req.params.ingredientId;
+    let toadd = req.body;
+    database.query(`SELECT * FROM ingredients WHERE code=${ingredient};`, (result)=>{
+        let previous = result;
+        database.query(`UPDATE ingredients SET stocks=${previous.stocks + toadd.stocks},stockvalue=${(previous.stocks + toadd.stocks)*previous.unitprice} WHERE code=${ingredient};`, function(err, result){
+            if(err) throw err;
+            console.log("Update complete.");
+        });
+        res.status(200).send('Ingredient has been updated successfully.');
+    });
+
+});
+
 module.exports = router;
