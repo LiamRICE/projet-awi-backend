16a916211358fb7e761f83cf3e868037672e3d5e
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-12-29 14:55:47 +0100

    fixed the problem?

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -16,10 +16,7 @@ router.get('/get/:ingredientId', (req, res, next) => {
 // requires input names code, libelle, unit, unitprice, stocks, stockvalue and allergene - make sure stocks are up to date (ie. current + new)
 router.post('/post', function (req, res){
     let ingredient = req.body;
-    database.query(`INSERT INTO ingredients VALUES (${ingredient.code},"${ingredient.libelle}","${ingredient.unit}",${ingredient.unitprice},${ingredient.stocks},${ingredient.stockvalue},${ingredient.allergene});`, function(err, result){
-        if(err) {
-            throw err;
-        }
+    database.query(`INSERT INTO ingredients VALUES (${ingredient.code},"${ingredient.libelle}","${ingredient.unit}",${ingredient.unitprice},${ingredient.stocks},${ingredient.stockvalue},${ingredient.allergene});`, function(result){
         console.log("Insert complete.");
         res.status(200).send('Ingredient has been added successfully.');
     });
