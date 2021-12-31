137143b509ffbe4b269b49035cc118f6424e6df7
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-12-31 15:30:16 +0100

    try to fix minor problem in techdoc get

--- routes/techdoc.js
+++ routes/techdoc.js
@@ -9,13 +9,14 @@ router.use(bodyParser.json());
 router.get('/get/all', (req, res, next) => {
     database.query(
         `SELECT * FROM 
-        (SELECT * FROM technicaldoc LEFT OUTER JOIN stepsindoc ON stepsindoc.docid = technicaldoc.id) AS header, 
-        (SELECT * FROM
-            (SELECT * FROM step LEFT OUTER JOIN stepusesingredient ON stepusesingredient.stepid = step.id) AS body 
-                LEFT OUTER JOIN 
-            (SELECT * FROM ingredients) AS detail
-        ON body.ingredientcode = detail.code) AS bottom
-        WHERE header.stepid = bottom.id;`
+(SELECT * FROM technicaldoc LEFT OUTER JOIN stepsindoc ON stepsindoc.docid = technicaldoc.id) AS header 
+LEFT OUTER JOIN 
+(SELECT * FROM
+(SELECT * FROM step LEFT OUTER JOIN stepusesingredient ON stepusesingredient.stepid = step.id) AS body 
+LEFT OUTER JOIN 
+(SELECT * FROM ingredients) AS detail
+ON body.ingredientcode = detail.code) AS bottom
+ON header.stepid = bottom.id;`
         , (result)=>{
             res.status(200).send(techdocService.toTechdocList(result));
         })
