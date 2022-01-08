766971b214ed183690cb77c7ae1495ffeb353740
AaronLaz <55982726+AaronLaz@users.noreply.github.com>
2022-01-08 22:13:05 +0100

    fix

--- routes/techdoc.js
+++ routes/techdoc.js
@@ -40,14 +40,15 @@ WHERE full.id=${req.params.techdocId} ORDER By rank ASC;`
         })
 })
 
+// peut provoquer soucis
 router.get('get/docid/:stepId', (req, res, next) => {
     database.query(`SELECT docid FROM stepsindoc WHERE stepid=${req.params.stepId};`
         ,(result) => {
             res.status(200).send(result);
         })
 })
-// test
-router.get('/get/tickets', (req, res, next) => {
+
+router.get('/tickets', (req, res, next) => {
   database.query(
         `SELECT technicaldoc.id, technicaldoc.name, ingredients.code, ingredients.libelle, ingredients.stocks, ingredients.allergene, SUM(stepusesingredient.quantity) AS quantite 
         FROM technicaldoc LEFT OUTER JOIN stepsindoc ON technicaldoc.id = stepsindoc.docid 
