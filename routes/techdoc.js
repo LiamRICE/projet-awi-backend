f887a3400a0a052399d782f5a92a8c05a150ff3b
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-01-10 09:42:46 +0100

    make unique id

--- routes/techdoc.js
+++ routes/techdoc.js
@@ -49,7 +49,7 @@ router.get('/get/docid/:stepId', (req, res, next) => {
 })
 
 router.get('/get/stepid', (req, res, next) => {
-    database.query(`SELECT * FROM step;`
+    database.query(`SELECT stepid FROM stepsindoc;`
         ,(result) => {
             res.status(200).send(result);
         })
